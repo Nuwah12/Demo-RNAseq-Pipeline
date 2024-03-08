9168da77fd8db9571977719f91341031abf2eca7
Noah Burget <51685498+Nuwah12@users.noreply.github.com>
2024-03-07 19:13:46 -0500

    Update step01_trim_galore.sh

--- step01_trim_galore.sh
+++ step01_trim_galore.sh
@@ -2,19 +2,19 @@
 # RNA-seq demo pipeline
 # Step 1: trim_galore
 ###############
-
+# Parameter check
 if [[ $# -ne 4 ]]; then
 	echo "Usage: ./trim_galore.sh <fastq1> <fastq2> <outdir> <cpu>"
 	exit 1
 fi
-
+# Define parameters
 fq1=${1}
 fq2=${2}
 outdir=${3}
 cpu=${4}
-
+# Define a name for our sample
 prefix=$(basename ${fq1} _R1.fastq.gz)
-
+# Call the tool with appropriate parameters
 trim_galore --fastqc \
 	-j ${cpu} \
 	-q 15 \
